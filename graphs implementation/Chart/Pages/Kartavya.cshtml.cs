b20ec57da3d0d44648d76abc97509edf7e8d6382
Kartavya Singh <kartavya.singh17@yahoo.com>
2024-08-25 21:27:09 -0400

    Few changes to add pages

---
+++ graphs implementation/Chart/Pages/Kartavya.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+public class Kartavya : PageModel
+{
+    public void OnGet()
+    {
+        // You can add any server-side logic here, if needed
+    }
+}
