5ca714f667823751216014cc43e74ecc84f8b9d2
Kartavya Singh <kartavya.singh17@yahoo.com>
2024-08-28 18:30:27 -0400

    Created the basic of the Home Page for FinVest

--- FinVest/obj/Debug/net7.0/FinVest.AssemblyInfo.cs
+++ FinVest/obj/Debug/net7.0/FinVest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinVest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df17c6785def338f30c79c824a8b50dc343e95e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinVest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinVest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
