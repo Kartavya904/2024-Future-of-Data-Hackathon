1e292f2e8ae6f2481d96f91501cd37e7819c7df3
Kartavya Singh <kartavya.singh17@yahoo.com>
2024-08-29 21:48:57 -0400

    Added StockWave

--- FinVest/obj/Debug/net8.0/FinVest.AssemblyInfo.cs
+++ FinVest/obj/Debug/net8.0/FinVest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinVest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4821440d64602c670b00d4ef4fdaefc000fd24a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55995ae81717e922cc85bcbcf900f8c43abab515")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinVest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinVest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ StockWave/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using StockWave.Models;
+
+namespace StockWave.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            // Passing an empty or default model to avoid null reference errors
+            var model = new Dictionary<string, Dictionary<string, string>>();
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
