9f4ba7b95c683235e74260a7a5e7f389afd4728a
Phani Bharadwaj Jarugumilli <phanibharadwaj10@gmail.com>
2024-09-02 17:35:15 -0400

    UI Changes for Finances.cshtml

--- StockWave/obj/Debug/net7.0/StockWave.AssemblyInfo.cs
+++ StockWave/obj/Debug/net7.0/StockWave.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StockWave")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9b408bef16a095330762608ab2eb5b29137b44f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a22c3da7316c7764cdaaa3dd53aaa02666a99e49")]
 [assembly: System.Reflection.AssemblyProductAttribute("StockWave")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StockWave")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
