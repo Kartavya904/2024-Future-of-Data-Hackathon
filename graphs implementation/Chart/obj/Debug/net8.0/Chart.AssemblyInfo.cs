019223cbdd1a363660e18306c8d5164aa2f3c54d
Kartavya Singh <kartavya.singh17@yahoo.com>
2024-09-02 18:34:49 -0400

    Final Merge Changes

--- graphs implementation/Chart/obj/Debug/net8.0/Chart.AssemblyInfo.cs
+++ graphs implementation/Chart/obj/Debug/net8.0/Chart.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chart")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f4ba7b95c683235e74260a7a5e7f389afd4728a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a27f07a08b81fb60cdcbd1dc528e9b8f388789b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chart")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chart")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
