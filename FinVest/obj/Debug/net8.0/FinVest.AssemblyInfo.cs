4c88a17c6a3fc24ff463e476d8d0875886cb73e3
Kartavya Singh <kartavya.singh17@yahoo.com>
2024-09-03 01:44:58 -0400

    Finances Page slightly fixed except 3D graphs

--- FinVest/obj/Debug/net8.0/FinVest.AssemblyInfo.cs
+++ FinVest/obj/Debug/net8.0/FinVest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinVest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb111d97d3502208af78e6d559b7e5e65da72275")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d9bb0092c7b76d102c36a3d23e2d44648f2913c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinVest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinVest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
